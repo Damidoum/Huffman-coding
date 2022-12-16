f23e72abe4227edcc9f87fc0edb316f34e404991
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2022-12-16 15:33:58 +0100

    new method

--- huffman-analyze.py
+++ huffman-analyze.py
@@ -13,15 +13,22 @@ class Language:
 
     def train(self, file: str):
         """Train the Language with a text"""
-        with open(file, "r") as file:
-            for line in file.readlines():
-                self.count["\n"] += 1
+        with open(file, "r") as f:
+            for line in f.readlines():
+                self.count["\\n"] += 1
                 for char in line[:-1]:
                     self.count[char] += 1
-            file.close()
+            f.close()
 
     def save_count_as_txt(self):
-        with open(self.name + "_count.txt", "w") as file:
+        with open(self.name + "_count.txt", "w") as f:
             for key, val in self.count.items():
-                file.write(key + " " + str(val) + "\n")
-            file.close()
+                f.write(key + ", " + str(val) + "\n")
+            f.close()
+
+    def load_count_txt(self, file: str):
+        with open(file, "r") as f:
+            for line in f.readlines():
+                key, val = line.split(", ")
+                self.count[key] += int(val)
+            f.close()
